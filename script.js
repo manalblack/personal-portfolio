c9dc681d6a03e8bfd515589969860ba4d8c1449d
manal-coder <103968256+manalblack@users.noreply.github.com>
2025-04-09 18:33:16 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// get the menu icon 
+const menu = document.getElementById('menu');
+const navLinks = document.querySelector('.links');
+const navBar = document.querySelector('.nav-bar');
+
+
+menu.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+    // navBar.classList.toggle('show-nav')
+})
