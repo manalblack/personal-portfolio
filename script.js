2d0e694c15120f491322f44419fb0e5778f7ac78
manal-coder <103968256+manalblack@users.noreply.github.com>
2025-04-10 21:45:55 -0800

    a little css editing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // get the menu icon 
-const menu = document.getElementById('menu');
+
+// const menu = document.getElementById('menu');
 const navLinks = document.querySelector('.links');
 const navBar = document.querySelector('.nav-bar');
 
