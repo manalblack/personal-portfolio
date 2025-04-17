606fcc5cd4b3f37ff3af8982e0f04a3b92aaddc7
manalblack <103968256+manalblack@users.noreply.github.com>
2025-04-16 21:22:54 -0800

    changed the lorem ipsum text

--- script.js
+++ script.js
@@ -4,9 +4,25 @@
 // const menu = document.getElementById('menu');
 const navLinks = document.querySelector('.links');
 const navBar = document.querySelector('.nav-bar');
+const buttons = document.querySelectorAll('.btn');
 
+const testBtn = document.querySelector('.test')
 
 menu.addEventListener('click', () => {
     navLinks.classList.toggle('show');
     // navBar.classList.toggle('show-nav')
 })
+
+testBtn.addEventListener('touchstart', () => {
+    testBtn.classList.add('touched');
+})
+
+testBtn.addEventListener('touchend', () => {
+    testBtn.classList.remove('touched');
+})
+
+// buttons.forEach(button => {
+//     button.addEventListener('touchstart', () =>{
+//         button.classList.add('test-touch')
+//     })
+// })
